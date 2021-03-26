92e69a61d9dda6d13961dedccc81c7988987c593
taowazi007 <taowazi@msn.com>
2021-03-26 18:56:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/openwrt/LEDE/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LEDE/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/UTC/Asia/Shanghai/g' openwrt/package/base-files/files/bin/config_generate
